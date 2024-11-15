f5f396dc66918bd3f0ee6adb6cf9d45640957ba3
isdabenx <blluch@gmail.com>
2024-11-15 03:30:26 +0100

    refactor: :recycle: update asset paths for improved resource management

--- lib/config/constants/assets.dart
+++ lib/config/constants/assets.dart
@@ -2,7 +2,7 @@ class Assets {
   static const String splashBackground = 'assets/images/splash_background.png';
   static const double splashBackgroundWidth = 626;
   static const double splashBackgroundHeight = 902;
-  static const String background = 'assets/images/background.jpg';
+  static const String background = 'assets/images/background.webp';
 
   static const String imagesTilePath = 'assets/images/tiles/';
   static const String imagesBoardgamePath = 'assets/images/boardgames/';
@@ -12,4 +12,12 @@ class Assets {
   static const String boardgamesJson = 'assets/initial_data/boardgames.json';
 
   static const String boardgameCacao = '${imagesBoardgamePath}cacao.webp';
+  static const String boardgameChocolatl =
+      '${imagesBoardgamePath}cacao_chocolatl.jpg';
+  static const String boardgameDiamante =
+      '${imagesBoardgamePath}cacao_diamante.webp';
+
+  static const String ruleCacaoPdf = 'assets/rules/cacao_rule_en.pdf';
+  static const String ruleCacaoOverviewPdf =
+      'assets/rules/cacao_overview_en.pdf';
 }
